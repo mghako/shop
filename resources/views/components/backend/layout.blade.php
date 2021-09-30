685389d939727462a62adb2d354a3588a5225c29
mghako <htetaungko@yahoo.com>
2021-10-01 00:48:25 +0630

    added fs icon

--- resources/views/components/backend/layout.blade.php
+++ resources/views/components/backend/layout.blade.php
@@ -17,7 +17,7 @@
   
     <header class="bg-white shadow">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between align-middle">
-        <x-form.link href="{{url()->previous()}}" :btn="'default'" style="display: block"> <- Back</x-form.link>
+        <x-form.link href="{{url()->previous()}}" :btn="'default'" style="display: block"> <i class="fas fa-arrow-left"></i> Back</x-form.link>
         <h1 class="md:text-3xl font-bold text-gray-900">
           @yield('title')
         </h1>
